1f136f6c387994066a9cde6751bef0d93e85db1a
Deive_Ex <feliperobos@gmail.com>
2024-04-06 02:28:10 -0300

    making class Static

--- Runtime/Statics/CollectionUtility.cs
+++ Runtime/Statics/CollectionUtility.cs
@@ -1,11 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 namespace DeiveEx.Utilities
 {
-    public class CollectionUtility
+    public static class CollectionUtility
     {
         public static bool IsNullOrEmpty<T>(IEnumerable<T> data) {
             return data == null || !data.Any();
