531546ebf56ac40ca7548beec8293eabea0fb304
Deive_Ex <feliperobos@gmail.com>
2024-05-23 21:34:53 -0300

    System Utility
    For things like Getting environment variables

---
+++ Runtime/Statics/SystemUtility.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace DeiveEx.Utilities
+{
+    public static class SystemUtility
+    {
+        public static bool HasCommandLineArgument(string key)
+        {
+            var args = Environment.GetCommandLineArgs();
+        
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == key)
+                    return true;
+            }
+
+            return false;
+        }
+        
+        public static bool GetCommandLineArgument(string key, out string value)
+        {
+            var args = Environment.GetCommandLineArgs();
+            value = "";
+        
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == key)
+                {
+                    if (args.Length > i)
+                    {
+                        value = args[i + 1];
+                        return true;
+                    }
+                    else
+                    {
+                        Debug.LogError($"{key} has no value!");
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
