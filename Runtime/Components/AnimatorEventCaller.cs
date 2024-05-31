2149952089ac30bab49828d3215ef22cab497993
Deive_Ex <feliperobos@gmail.com>
2024-05-31 15:13:57 -0300

    AnimatiorEventCaller now uses a simple class
    This should be safer (?) than using SerializedMonoBehavior

--- Runtime/Components/AnimatorEventCaller.cs
+++ Runtime/Components/AnimatorEventCaller.cs
@@ -1,29 +1,47 @@
-#if ODIN_INSPECTOR
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
+#if ODIN_INSPECTOR
 using Sirenix.OdinInspector;
+#endif
 
 namespace DeiveEx.Utilities
 {
-    public class AnimatorEventCaller : SerializedMonoBehaviour
+    public class AnimatorEventCaller : MonoBehaviour
     {
-        [SerializeField] Dictionary<string, List<UnityEvent>> _events = new();
+        [Serializable]
+        public class AnimationEvent
+        {
+#if ODIN_INSPECTOR
+            [TableColumnWidth(150, Resizable = false)]
+#endif
+            public string EventName;
+            [SerializeReference]
+            public List<UnityEvent> EventList;
+        }
+        
+#if ODIN_INSPECTOR
+        [TableList]
+#endif
+        [SerializeField] List<AnimationEvent> _events = new();
 
         public void CallEvent(string eventID)
         {
-            if (!_events.TryGetValue(eventID, out var eventList))
+            var animEvent = _events.FirstOrDefault(x => x.EventName == eventID);
+            
+            if(animEvent == null)
             {
                 Debug.LogWarning($"Event with ID \"{eventID}\" not found");
                 return;
             }
-
-            foreach (var e in eventList)
+            
+            foreach (var e in animEvent.EventList)
             {
                 e.Invoke();
             }
         }
     }
 }
-#endif
