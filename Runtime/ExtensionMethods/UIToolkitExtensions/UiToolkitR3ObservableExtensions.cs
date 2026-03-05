83da6c6db85ea09c38944ce4be5fb70a7fd55a01
Deive_Ex <feliperobos@gmail.com>
2026-03-04 21:40:28 -0300

    UI Toolkit R3 Observable Extensions

---
+++ Runtime/ExtensionMethods/UIToolkitExtensions/UiToolkitR3ObservableExtensions.cs
@@ -0,0 +1,23 @@
+using R3;
+using UnityEngine.UIElements;
+
+namespace DeiveEx.Utilities
+{
+    public static class UIToolkitR3ObservableExtensions
+    {
+        public static Observable<Unit> OnClickAsObservable(this Button button)
+        {
+            return Observable.FromEvent(
+                h => button.clicked += h,
+                h => button.clicked -= h);
+        }
+
+        public static Observable<ChangeEvent<T>> OnValueChangedAsObservable<T>(this INotifyValueChanged<T> element)
+        {
+            return Observable.FromEvent<EventCallback<ChangeEvent<T>>, ChangeEvent<T>>(
+                h => evt => h(evt),
+                h => element.RegisterValueChangedCallback(h),
+                h => element.UnregisterValueChangedCallback(h));
+        }
+    }
+}
